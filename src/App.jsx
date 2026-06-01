3deec73c247050433271b3fc02d6e323900f5cce
Rekeya32 <chocolate.skyys@gmail.com>
2026-06-01 04:03:38 +0000

    Stage 18C Add Domain Vault

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import FounderTierRules from './components/FounderTierRules/FounderTierRules';
 import SignupRequest from './components/SignupRequest/SignupRequest';
 import AssetVault from './components/AssetVault/AssetVault';
 import CheckoutRoom from './components/CheckoutRoom/CheckoutRoom';
+import DomainVault from './components/DomainVault/DomainVault';
 
 function App() {
   const [currentView, setCurrentView] = useState('entryGate');
@@ -53,6 +54,10 @@ function App() {
 
     if (currentView === 'checkoutRoom') {
       return <CheckoutRoom onReturn={() => setCurrentView('entryGate')} />;
+    }
+
+    if (currentView === 'domainVault') {
+      return <DomainVault onReturn={() => setCurrentView('entryGate')} />;
     }
 
     return (
@@ -165,6 +170,14 @@ function App() {
                 className="px-4 py-2 border border-green-500 text-green-300 rounded hover:bg-green-950"
               >
                 Checkout
+              </button>
+
+              <button
+                type="button"
+                onClick={() => setCurrentView('domainVault')}
+                className="px-4 py-2 border border-yellow-500 text-yellow-300 rounded hover:bg-yellow-950"
+              >
+                Domains
               </button>
             </div>
           </div>
