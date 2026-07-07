ae2852333b05ed841d9cb1572ca8816bc473dcc1
chocolateskyys-tech <chocolate.skyys@gmail.com>
2026-07-07 05:56:46 -0400

    Ignore backup folders in lint

--- eslint.config.js
+++ eslint.config.js
@@ -5,9 +5,16 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores([
+    'dist',
+    'node_modules',
+    'src_BACKUP_BEFORE_FINAL_BRAND_CLEANUP_20260703_0651',
+    'src_BACKUP_BEFORE_FINAL_BRAND_CLEANUP_20260703_0651/**',
+    'ZIP_RESTORE_REVIEW',
+    'ZIP_RESTORE_REVIEW/**',
+  ]),
   {
-    files: ['**/*.{js,jsx}'],
+    files: ['src/**/*.{js,jsx}', '*.config.js'],
     extends: [
       js.configs.recommended,
       reactHooks.configs.flat.recommended,
@@ -16,6 +23,12 @@ export default defineConfig([
     languageOptions: {
       globals: globals.browser,
       parserOptions: { ecmaFeatures: { jsx: true } },
+    },
+    rules: {
+      'no-unused-vars': ['error', {
+        varsIgnorePattern: '^React$',
+        argsIgnorePattern: '^_',
+      }],
     },
   },
 ])
